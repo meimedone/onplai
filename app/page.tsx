4270f23fc32988ff829788488bf94040ad592ada
meimedone <dominic.meister@meimed.ch>
2026-06-09 13:39:33 +0000

    Add initial app structure

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>ONplAI</h1>
+      <p>Release 1 Plattform läuft.</p>
+    </main>
+  );
+}
